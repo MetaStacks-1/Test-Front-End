2b91414a10d6dc9c82c0bfbdb4a9c49931816978
Squill-Bob <blockstaffconsulting@gmail.com>
2021-11-29 11:14:48 -0600

    commit on main

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,65 @@
-import Head from 'next/head'
-import Web3 from 'web3'
-import { useState, useEffect } from 'react'
+import Head from "next/head";
+import Web3 from "web3";
+import { useState, useEffect } from "react";
 
-import { ADDRESS, ABI } from '../config.js'
-import Image from 'next/image'
-import MintModel from '../src/components/mintModel'
-import banner from '../public/img/banner.jpg'
+import { ADDRESS, ABI } from "../config.js";
+import Image from "next/image";
+import MintModel from "../src/components/mintModel";
+import banner from "../public/img/banner.jpg";
 
 export default function Mint() {
   // FOR WALLET
-  const [signedIn, setSignedIn] = useState(false)
+  const [signedIn, setSignedIn] = useState(false);
 
-  const [metaConnect, setMetaConnect] = useState(false)
+  const [metaConnect, setMetaConnect] = useState(false);
 
-  const [walletAddress, setWalletAddress] = useState(null)
+  const [walletAddress, setWalletAddress] = useState(null);
 
   // FOR MINTING
-  const [how_many_sndwchs, set_how_many_sndwchs] = useState(1)
+  const [how_many_Tokens, set_how_many_Tokens] = useState(1);
 
-  const [sndwchContract, setsndwchContract] = useState(null)
+  const [TestContract, setTestContract] = useState(null);
 
   // INFO FROM SMART Contract
 
-  const [totalSupply, setTotalSupply] = useState(0)
-  const [counter, setCounter] = useState(0)
-  const [privatesaleStarted, setPrivateSaleStarted] = useState(false)
-  const [publicsaleStarted, setPublicSaleStarted] = useState(false)
-  const [sndwchPrice, setsndwchPrice] = useState(0)
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [privatesaleStarted, setPrivateSaleStarted] = useState(false);
+  const [publicsaleStarted, setPublicSaleStarted] = useState(false);
+  const [TestPrice, setTestPrice] = useState(0);
 
   const onPublicMint = async () => {
-    if(counter > 0){
-      await mintSndwch(counter)
-      setCounter(0)
+    if (counter > 0) {
+      await mintToken(counter);
+      setCounter(0);
     }
-  }
+  };
 
   const onPrivateMint = async () => {
-    await mintAllowList(counter)
-    setCounter(0)
-  }
+    await mintAllowList(counter);
+    setCounter(0);
+  };
   const Increment = () => {
     if (privatesaleStarted) {
       if (counter < 2) {
-        setCounter(counter + 1)
+        setCounter(counter + 1);
       }
     } else {
       if (counter < 3) {
-        setCounter(counter + 1)
+        setCounter(counter + 1);
       }
     }
-  }
+  };
   const Decrement = () => {
-    if (!(counter <= 0)) setCounter(counter - 1)
-  }
+    if (!(counter <= 0)) setCounter(counter - 1);
+  };
 
   async function signIn() {
-    if (typeof window.web3 !== 'undefined') {
+    if (typeof window.web3 !== "undefined") {
       // Use existing gateway
-      window.web3 = new Web3(window.ethereum)
+      window.web3 = new Web3(window.ethereum);
     } else {
-      alert('No Ethereum interface injected into browser. Read-only access')
+      alert("No Ethereum interface injected into browser. Read-only access");
     }
 
     window.ethereum
@@ -69,91 +69,91 @@ export default function Mint() {
           .getNetworkType()
           // checks if connected network is mainnet (change this to rinkeby if you wanna test on testnet)
           .then((network) => {
-            console.log(network)
-            if (network != 'main') {
+            console.log(network);
+            if (network != "main") {
               alert(
-                'You are on ' +
+                "You are on " +
                   network +
                   " network. Change network to mainnet or you won't be able to do anything here"
-              )
+              );
             }
-          })
-        let wallet = accounts[0]
-        console.log('wallet', wallet)
-        setWalletAddress(wallet)
-        setSignedIn(true)
-        callContractData(wallet)
+          });
+        let wallet = accounts[0];
+        console.log("wallet", wallet);
+        setWalletAddress(wallet);
+        setSignedIn(true);
+        callContractData(wallet);
       })
       .catch(function (error) {
         // Handle error. Likely the user rejected the login
-        console.error(error)
-      })
+        console.error(error);
+      });
   }
 
   //
 
   async function signOut() {
-    setSignedIn(false)
+    setSignedIn(false);
   }
 
   async function callContractData(wallet) {
-    const sndwchContract = new window.web3.eth.Contract(ABI, ADDRESS)
-    const allowedListActive = await sndwchContract.methods
+    const TestContract = new window.web3.eth.Contract(ABI, ADDRESS);
+    const allowedListActive = await TestContract.methods
       .isAllowListActive()
-      .call()
-    const saleActive = await sndwchContract.methods.saleIsActive().call()
-    const totalSupply = await sndwchContract.methods.totalSupply().call()
-    const Price = await sndwchContract.methods.SndwchPrice().call()
+      .call();
+    const saleActive = await TestContract.methods.saleIsActive().call();
+    const totalSupply = await TestContract.methods.totalSupply().call();
+    const Price = await TestContract.methods.TokenPrice().call();
 
-    setsndwchContract(sndwchContract)
-    setPrivateSaleStarted(allowedListActive)
-    setPublicSaleStarted(saleActive)
-    setTotalSupply(totalSupply)
-    setsndwchPrice(Price)
+    setTestContract(TestContract);
+    setPrivateSaleStarted(allowedListActive);
+    setPublicSaleStarted(saleActive);
+    setTotalSupply(totalSupply);
+    setTokenPrice(Price);
   }
 
-  async function mintSndwch(how_many_sndwchs) {
-    if (sndwchContract) {
-      console.log('hello')
-      const price = Number(sndwchPrice) * how_many_sndwchs
-      const gasAmount = await sndwchContract.methods
-        .mintSndwch(how_many_sndwchs)
-        .estimateGas({ from: walletAddress, value: price })
+  async function mintToken(how_many_Tokens) {
+    if (TestContract) {
+      console.log("hello");
+      const price = Number(TokenPrice) * how_many_Tokens;
+      const gasAmount = await TestContract.methods
+        .mintToken(how_many_Tokens)
+        .estimateGas({ from: walletAddress, value: price });
 
-      sndwchContract.methods
-        .mintSndwch(how_many_sndwchs)
+      TestContract.methods
+        .mintToken(how_many_Tokens)
         .send({ from: walletAddress, value: price, gas: String(gasAmount) })
-        .on('transactionHash', function (hash) {
-          console.log('transactionHash', hash)
-          alert('Transaction In Proccess Please Check Metamask For Details')
-        })
+        .on("transactionHash", function (hash) {
+          console.log("transactionHash", hash);
+          alert("Transaction In Proccess Please Check Metamask For Details");
+        });
     } else {
-      console.log('Wallet not connected')
-      alert('Wallet not connected')
+      console.log("Wallet not connected");
+      alert("Wallet not connected");
     }
   }
 
-  async function mintAllowList(how_many_sndwchs) {
-    if (sndwchContract) {
-      const price = Number(sndwchPrice) * how_many_sndwchs
-      const gasAmount = await sndwchContract.methods
-        .mintAllowList(how_many_sndwchs)
-        .estimateGas({ from: walletAddress, value: price })
+  async function mintAllowList(how_many_Tokens) {
+    if (TestContract) {
+      const price = Number(TokenPrice) * how_many_Tokens;
+      const gasAmount = await TestContract.methods
+        .mintAllowList(how_many_Tokens)
+        .estimateGas({ from: walletAddress, value: price });
 
-      await sndwchContract.methods
-        .mintAllowList(how_many_sndwchs)
+      await TestContract.methods
+        .mintAllowList(how_many_Tokens)
         .send({ from: walletAddress, value: price, gas: String(gasAmount) })
-        .on('transactionHash', function (hash) {
-          console.log('transactionHash', hash)
-          alert('Transaction In Proccess Please Check Metamask For Details')
-        })
+        .on("transactionHash", function (hash) {
+          console.log("transactionHash", hash);
+          alert("Transaction In Proccess Please Check Metamask For Details");
+        });
     } else {
-      console.log('Wallet not connected')
-      alert('Wallet not connected')
+      console.log("Wallet not connected");
+      alert("Wallet not connected");
     }
   }
 
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     return (
       <div id="appmain">
         <Head>
@@ -164,17 +164,17 @@ export default function Mint() {
             rel="stylesheet"
           />
         </Head>
-          <MintModel
-            signIn={signIn}
-            signedIn={signedIn}
-            onClick={onPublicMint}
-            Increment={Increment}
-            Decrement={Decrement}
-            counter={counter}
-            totalSupply={totalSupply}
-          />
+        <MintModel
+          signIn={signIn}
+          signedIn={signedIn}
+          onClick={onPublicMint}
+          Increment={Increment}
+          Decrement={Decrement}
+          counter={counter}
+          totalSupply={totalSupply}
+        />
       </div>
-    )
+    );
   }
-  return <div>Loading...</div>
+  return <div>Loading...</div>;
 }
